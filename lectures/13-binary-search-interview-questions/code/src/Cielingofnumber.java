de695f8403dc49f13a5a5b162476bd68fc6c8f56
rishimanjunath15 <rishimanjunath15@gmail.com>
2026-02-11 22:05:49 +0530

    binary-search-iq-01

---
+++ lectures/13-binary-search-interview-questions/code/src/Cielingofnumber.java
@@ -0,0 +1,28 @@
+public class Cielingofnumber {
+    public static void main(String[] args) {
+        int[] arr = {-18,-12,-4,0,2,3,4,15,16,18,22,45,89};
+        int target = 17;
+
+        int res = cieling(arr,target);
+        System.out.println(res);
+    }
+
+    static int cieling(int arr[],int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+            int mid = start + (end - start)/2;
+
+            if (target > arr[mid]){
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return start;
+
+    }
+}
---
+++ lectures/13-binary-search-interview-questions/code/src/Floor.java
@@ -0,0 +1,30 @@
+public class Floor {
+        public static void main(String[] args) {
+            int[] arr = {0,2,3,4,15,16,18,22,45,89};
+            int target = 17;
+
+            int res = floorofnumber(arr,target);
+            System.out.println(res);
+        }
+
+        static int floorofnumber(int arr[],int target){
+            int start = 0;
+            int end = arr.length - 1;
+
+            while(start<=end){
+                int mid = start + (end - start)/2;
+
+                if (target > arr[mid]){
+                    start = mid + 1;
+                } else if (target < arr[mid]) {
+                    end = mid - 1;
+                }else{
+                    return mid;
+                }
+            }
+            return end;
+
+        }
+    }
+
+
